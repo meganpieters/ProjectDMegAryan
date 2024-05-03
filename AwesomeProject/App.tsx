6646b5c9aba224cbae3363fc4db358de09f0356a
Megan <meganpieters3@gmail.com>
2024-05-03 16:05:24 +0200

    uhmmm

--- AwesomeProject/App.tsx
+++ AwesomeProject/App.tsx
@@ -3,10 +3,12 @@
  * https://github.com/facebook/react-native
  *
  * @format
+ * @flow strict-local
  */
 
 import React from 'react';
-import type {PropsWithChildren} from 'react';
+import type { ReactNode } from 'react';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -24,95 +26,48 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Auth from './Auth';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
+const Section = ({ children, title }: { children: ReactNode, title: string }): ReactNode => {
   const isDarkMode = useColorScheme() === 'dark';
+  const textColor = isDarkMode ? Colors.white : Colors.black;
+  const backgroundColor = isDarkMode ? Colors.light : Colors.dark;
+
   return (
     <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <Text style={[styles.sectionTitle, { color: textColor }]}>{title}</Text>
+      <Text style={[styles.sectionDescription, { color: backgroundColor }]}>{children}</Text>
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
+const App: () => ReactNode = () => {
   const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const backgroundStyle = { backgroundColor: isDarkMode ? Colors.darker : Colors.lighter };
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={{ backgroundColor: isDarkMode ? Colors.black : Colors.white }}>
+          <Section title="Step Zero">Use <Text style={styles.highlight}>Okta</Text> for authentication.</Section>
+          <Auth />
+          <Section title="Step One">Edit <Text style={styles.highlight}>App.tsx</Text> to change this screen and then come back to see your edits.</Section>
+          <Section title="See Your Changes"><ReloadInstructions /></Section>
+          <Section title="Debug"><DebugInstructions /></Section>
+          <Section title="Learn More">Read the docs to discover what to do next:<LearnMoreLinks /></Section>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  sectionContainer: { marginTop: 32, paddingHorizontal: 24 },
+  sectionTitle: { fontSize: 24, fontWeight: '600' },
+  sectionDescription: { marginTop: 8, fontSize: 18, fontWeight: '400' },
+  highlight: { fontWeight: '700' },
 });
 
 export default App;
