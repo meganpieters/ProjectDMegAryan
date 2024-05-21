e4747355f634ef198ebad059ed0ebb9d184f2c02
AryanJadoenath <1061627@hr.nl>
2024-05-21 02:13:39 +0200

    home responsive gemaakt

--- AwesomeProject/screens/Home.tsx
+++ AwesomeProject/screens/Home.tsx
@@ -3,6 +3,7 @@ import { StyleSheet, View, Text, Image, Pressable } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { useNavigation, ParamListBase } from "@react-navigation/native";
 import { Color, Border, FontSize, FontFamily } from "../GlobalStyles";
+import { horizontalScale, verticalScale, moderateScale } from '../Metrics';
 
 const Home = () => {
   const navigation = useNavigation<StackNavigationProp<ParamListBase>>();
@@ -19,8 +20,7 @@ const Home = () => {
         resizeMode="cover"
         source={require("../assets/rectangle-7.png")}
       />
-      <Text style={[styles.hoursLeft, styles.textFlexBox]}>{`2,5 hours left
-`}</Text>
+      <Text style={[styles.hoursLeft, styles.textFlexBox]}>2,5 hours left</Text>
       <View style={[styles.rectangleView, styles.homeInnerPosition]} />
       <Pressable
         style={[styles.framePressable, styles.frameLayout]}
@@ -42,16 +42,13 @@ const Home = () => {
       </Pressable>
       <Pressable style={[styles.rectangleGroup, styles.frameChildLayout]}>
         <View style={[styles.frameItem, styles.framePosition]} />
-        <Text style={[styles.stopCharging, styles.requestTypo]}>{`Stop 
-Charging`}</Text>
+        <Text style={[styles.stopCharging, styles.requestTypo]}>Stop Charging</Text>
         <View style={styles.frameInner} />
       </Pressable>
       <Text style={[styles.text, styles.textFlexBox]}>40%</Text>
-      <Text
-        style={[styles.teslaModelX, styles.teslaModelXFlexBox]}
-      >{`Tesla Model X
-Rotterdam
-ID #2005`}</Text>
+      <Text style={[styles.teslaModelX, styles.teslaModelXFlexBox]}>
+        Tesla Model X Rotterdam ID #2005
+      </Text>
       <Image
         style={[styles.iconCheck, styles.iconLayout]}
         resizeMode="cover"
@@ -105,7 +102,7 @@ ID #2005`}</Text>
 
 const styles = StyleSheet.create({
   homeShadowBox: {
-    width: 351,
+    width: horizontalScale(351), // Use horizontalScale for width
     borderWidth: 1,
     borderColor: Color.colorBlack,
     borderStyle: "solid",
@@ -128,10 +125,10 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   homeInnerPosition: {
-    height: 56,
+    height: verticalScale(56), // Use verticalScale for height
     borderRadius: Border.br_11xl,
-    left: 77,
-    top: 447,
+    left: horizontalScale(50), // Use horizontalScale for left
+    top: verticalScale(435), // Use verticalScale for top
     position: "absolute",
   },
   textFlexBox: {
@@ -139,7 +136,7 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
   frameLayout: {
-    height: 62,
+    height: verticalScale(62), // Use verticalScale for height
     position: "absolute",
   },
   framePosition: {
@@ -147,8 +144,8 @@ const styles = StyleSheet.create({
     top: 0,
   },
   frameChildLayout: {
-    width: 147,
-    height: 62,
+    width: horizontalScale(147), // Use horizontalScale for width
+    height: verticalScale(62), // Use verticalScale for height
     position: "absolute",
   },
   iconLayout: {
@@ -163,54 +160,57 @@ const styles = StyleSheet.create({
     fontWeight: "700",
   },
   imageLayout: {
-    height: 42,
-    width: 45,
+    height: verticalScale(42), // Use verticalScale for height
+    width: horizontalScale(45), // Use horizontalScale for width
   },
   homeChild1Position: {
-    width: 430,
+    width: horizontalScale(430), // Use horizontalScale for width
     left: 0,
     position: "absolute",
   },
+  // home knopen enz om de 1 of andere reden zijn de home en charger knop samen...
   image26Position: {
-    top: 867,
+    top: verticalScale(780), // Use verticalScale for top
     position: "absolute",
   },
+  // Grote blauwe rechthoek
   homeChild: {
-    marginTop: -329,
-    marginLeft: -175,
+    marginTop: -verticalScale(300), // Use verticalScale for marginTop
+    marginLeft: -horizontalScale(175), // Use horizontalScale for marginLeft
     backgroundColor: Color.colorCornflowerblue,
-    height: 418,
+    height: verticalScale(418), // Use verticalScale for height
   },
+  // donker blauwe charger available rechthoek
   homeItem: {
-    marginTop: 149,
-    marginLeft: -178,
+    marginTop: verticalScale(135), // Use verticalScale for marginTop
+    marginLeft: -horizontalScale(178), // Use horizontalScale for marginLeft
     backgroundColor: Color.colorDarkslateblue,
-    height: 78,
+    height: verticalScale(78), // Use verticalScale for height
   },
   charger1Available: {
-    top: 639,
-    left: 121,
+    top: verticalScale(578), // Use verticalScale for top
+    left: horizontalScale(121), // Use horizontalScale for left
     fontFamily: FontFamily.inriaSansBold,
     fontWeight: "700",
     textAlign: "center",
     fontSize: FontSize.size_5xl,
   },
   homeInner: {
-    width: 282,
+    width: horizontalScale(282), // Use horizontalScale for width
   },
   hoursLeft: {
-    top: 466,
-    left: 242,
+    top: verticalScale(455), // Use verticalScale for top
+    left: horizontalScale(200), // Use horizontalScale for left
     color: Color.colorBlack,
-    width: 152,
-    height: 24,
+    width: horizontalScale(152), // Use horizontalScale for width
+    height: verticalScale(24), // Use verticalScale for height
     fontSize: FontSize.size_base,
     fontFamily: FontFamily.inriaSansBold,
     fontWeight: "700",
   },
   rectangleView: {
     backgroundColor: "#d3b934",
-    width: 143,
+    width: horizontalScale(143), // Use horizontalScale for width
   },
   frameChild: {
     borderRadius: Border.br_9xs,
@@ -226,14 +226,15 @@ const styles = StyleSheet.create({
     bottom: "25.81%",
     left: "26.83%",
   },
+  // request icoon
   rectangleParent: {
-    width: 82,
-    height: 62,
+    width: horizontalScale(82), // Use horizontalScale for width
+    height: verticalScale(62), // Use verticalScale for height
     position: "absolute",
   },
   request: {
-    top: 21,
-    left: 73,
+    top: verticalScale(19), // Use verticalScale for top
+    left: horizontalScale(70), // Use horizontalScale for left
     fontSize: FontSize.size_base,
     fontFamily: FontFamily.inriaSansBold,
     fontWeight: "700",
@@ -241,64 +242,71 @@ const styles = StyleSheet.create({
     textAlign: "left",
   },
   frameWrapper: {
-    height: 62,
+    height: verticalScale(62), // Use verticalScale for height
     position: "absolute",
-    width: 132,
+    width: horizontalScale(132), // Use horizontalScale for width
   },
+  // request knop
   framePressable: {
-    left: 51,
-    width: 132,
-    height: 62,
-    top: 728,
+    left: horizontalScale(40), // Use horizontalScale for left
+    width: horizontalScale(132), // Use horizontalScale for width
+    height: verticalScale(62), // Use verticalScale for height
+    top: verticalScale(650), // Use verticalScale for top
   },
+  // stop charging knop
   frameItem: {
-    borderRadius: 8,
+    borderRadius: moderateScale(8), // Use moderateScale for borderRadius
     backgroundColor: Color.colorFirebrick,
-    width: 135,
-    height: 62,
+    width: horizontalScale(135), // Use horizontalScale for width
+    height: verticalScale(62), // Use verticalScale for height
     position: "absolute",
   },
+  // stop charging tekst
   stopCharging: {
-    top: 12,
-    left: 59,
-    width: 63,
-    height: 37,
+    top: verticalScale(20), // Use verticalScale for top
+    left: horizontalScale(54), // Use horizontalScale for left
+    width: horizontalScale(63), // Use horizontalScale for width
+    height: verticalScale(37), // Use verticalScale for height
     textAlign: "center",
     color: Color.colorWhite,
     position: "absolute",
   },
+  // stop charging knop
   frameInner: {
-    top: 18,
-    left: 17,
-    width: 26,
-    height: 26,
+    top: verticalScale(18), // Use verticalScale for top
+    left: horizontalScale(17), // Use horizontalScale for left
+    width: horizontalScale(26), // Use horizontalScale for width
+    height: verticalScale(26), // Use verticalScale for height
     position: "absolute",
     backgroundColor: Color.colorWhite,
   },
+  // stop charging vierkant
   rectangleGroup: {
-    left: 237,
-    top: 728,
+    left: horizontalScale(220), // Use horizontalScale for left
+    top: verticalScale(650), // Use verticalScale for top
   },
+  // batterij percentage
   text: {
-    top: 462,
-    left: 95,
+    top: verticalScale(450), // Use verticalScale for top
+    left: horizontalScale(95), // Use horizontalScale for left
     fontFamily: FontFamily.inderRegular,
-    width: 142,
-    height: 25,
+    width: horizontalScale(142), // Use horizontalScale for width
+    height: verticalScale(25), // Use verticalScale for height
     textShadowColor: "rgba(0, 0, 0, 0.25)",
     textShadowOffset: {
       width: 0,
-      height: 4,
+      height: verticalScale(4), // Use verticalScale for height
     },
-    textShadowRadius: 4,
+    textShadowRadius: horizontalScale(4), // Use horizontalScale for radius
     color: Color.colorWhite,
     textAlign: "left",
     fontSize: FontSize.size_5xl,
   },
+  // auto naam
   teslaModelX: {
-    top: 172,
-    left: 133,
-    width: 162,
+    top: verticalScale(150), // Use verticalScale for top
+    left: horizontalScale(112), // Use horizontalScale for left
+    width: horizontalScale(162), // Use horizontalScale for width
     fontFamily: FontFamily.inriaSansBold,
     fontWeight: "700",
     textAlign: "center",
@@ -312,59 +320,68 @@ const styles = StyleSheet.create({
     bottom: "28.33%",
     left: "16.98%",
   },
+  // oplaad icoon
   image24: {
-    top: 866,
-    left: 192,
+    top: verticalScale(866), // Use verticalScale for top
+    left: horizontalScale(192), // Use horizontalScale for left
     position: "absolute",
   },
+  // Onderbalk
   homeChild1: {
-    top: 842,
+    top: verticalScale(760), // Use verticalScale for top
     backgroundColor: Color.colorDodgerblue,
-    height: 101,
+    height: verticalScale(101), // Use verticalScale for height
   },
+  // gebruikers knop
   image25Icon: {
-    width: 44,
-    height: 39,
-    left: 192,
+    width: horizontalScale(44), // Use horizontalScale for width
+    height: verticalScale(39), // Use verticalScale for height
+    left: horizontalScale(192), // Use horizontalScale for left
   },
   icon: {
     height: "100%",
     width: "100%",
   },
+  // oplaad knop
   image26: {
-    left: 79,
-    height: 42,
-    width: 45,
+    left: horizontalScale(79), // Use horizontalScale for left
+    height: verticalScale(42), // Use verticalScale for height
+    width: horizontalScale(45), // Use horizontalScale for width
   },
+  // gebruikers knop in onderste balk
   image27: {
-    left: 315,
-    top: 865,
-    width: 37,
-    height: 43,
+    left: horizontalScale(300), // Use horizontalScale for left
+    top: verticalScale(778), // Use verticalScale for top
+    width: horizontalScale(37), // Use horizontalScale for width
+    height: verticalScale(43), // Use verticalScale for height
     position: "absolute",
   },
+  // rechthoek boven
   rectangleIcon: {
-    height: 90,
+    height: verticalScale(90), // Use verticalScale for height
     top: 0,
-    width: 430,
+    width: horizontalScale(430), // Use horizontalScale for width
   },
+  // Tesla auto
   afbeelding3Icon: {
-    top: 287,
-    left: 74,
-    width: 289,
-    height: 117,
+    top: verticalScale(270), // Use verticalScale for top
+    left: horizontalScale(55), // Use horizontalScale for left
+    width: horizontalScale(289), // Use horizontalScale for width
+    height: verticalScale(117), // Use verticalScale for height
     position: "absolute",
   },
+  // Schuberg icoon
   image30Icon: {
-    top: 7,
-    left: 22,
-    width: 381,
-    height: 75,
+    top: verticalScale(9), // Use verticalScale for top
+    left: horizontalScale(13), // Use horizontalScale for left
+    width: horizontalScale(381), // Use horizontalScale for width
+    height: verticalScale(75), // Use verticalScale for height
     position: "absolute",
   },
+  // home knop
   home: {
     flex: 1,
-    height: 932,
+    height: verticalScale(932), // Use verticalScale for height
     overflow: "hidden",
     width: "100%",
     backgroundColor: Color.colorWhite,
